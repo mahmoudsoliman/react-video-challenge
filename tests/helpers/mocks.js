9aba956f83406679e772873f93b1eb0c2f3a3074
Mahmoud Soliman <mahmoudsoliman@Mahmouds-MacBook-Pro.local>
2021-03-02 14:49:27 +0200

    videos grid tests

--- tests/helpers/mocks.js
+++ tests/helpers/mocks.js
@@ -1,24 +1,23 @@
-import * as videoService from '../../src/services/videoService'
+import * as searchActions from '../../src/actions/searchActions'
 
 export const mockVideoSearch = () => {
-  videoService.search = jest.fn().mockImplementation(() => {
-    return [
-      {
-        id: '1234',
-        title: 'Video 1',
-        description: 'test vidoe 1',
-        thumbnail: '',
-        url: `https://www.youtube.com/watch?v=1234`
-      },
-      {
-        id: '1742',
-        title: 'Video 2',
-        description: 'test vidoe 2',
-        thumbnail: '',
-        url: `https://www.youtube.com/watch?v=1742`
-      }
-    ]
-  })
+  searchActions.searchExecuted = jest.fn()
+  return [
+    {
+      id: '1234',
+      title: 'Video 1',
+      description: 'test vidoe 1',
+      thumbnail: 'testimage.png',
+      url: `https://www.youtube.com/watch?v=1234`
+    },
+    {
+      id: '1742',
+      title: 'Video 2',
+      description: 'test vidoe 2',
+      thumbnail: 'testimage.png',
+      url: `https://www.youtube.com/watch?v=1742`
+    }
+  ]
 }
 
 export const mockVideoObject = () => {
