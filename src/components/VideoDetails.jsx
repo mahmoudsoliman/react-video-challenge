3e5f630450e2e3dc0beddb30a4b4952e5c91e1fd
Mahmoud Soliman <mahmoudsoliman@Mahmouds-MacBook-Pro.local>
2021-03-30 14:00:16 +0200

    fix video details navigation and related videos

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -3,32 +3,41 @@ import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import VideosList from './VideosList'
 import VideoPlayer from './VideoPlayer'
+import * as videoService from '../services/videoService'
 
-class VideoDetails extends Component {
-  getCurrentVideo = (videoId) => {
-    return this.props.videos.find(video => video.id === videoId)
+export default class VideoDetails extends Component {
+  state = {
+    video: null,
+    relatedVideos: []
+  }
+  
+  async componentDidMount() {
+    const videoId = this.props.match.params.id
+    const video = await videoService.getVideoById(videoId)
+    if(!video) return <Redirect to="/"/>
+    const relatedVideos = await videoService.getRelatedVideos(videoId)
+    this.setState({
+      video,
+      relatedVideos
+    })
   }
 
   render() {
-    const videoId = this.props.match.params.id
-    const video = this.getCurrentVideo(videoId)
-    if(!video) return <Redirect to="/"/>
+    const {
+      video,
+      relatedVideos
+    } = this.state
     return (
+      video && relatedVideos?(
       <div className="row m-2">
         <div className="col-8">
           <VideoPlayer video={video} />
         </div>
         <div className="col-4">
-          <VideosList videos={this.props.videos}/>
+          <VideosList videos={relatedVideos}/>
         </div>
       </div>
+      ) : ""
     )
   }
 }
-
-const mapStateToProps = state => ({
-  videos: state.search.videos,
-  query: state.search.query
-})
-
-export default connect(mapStateToProps)(VideoDetails)
--- src/services/videoService.js
+++ src/services/videoService.js
@@ -21,3 +21,47 @@ export const search = async (query) => {
     }
   })
 }
+
+export const getVideoById = async (videoId) => {
+  const res = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+    params: {
+      key: process.env.REACT_APP_YOUTUBE_API_KEY,
+      part: 'snippet',
+      id: videoId
+    }
+  })
+
+  if(res.data.items.length === 0) return null
+
+  return res.data.items.map(item => {
+    return {
+      id: item.id,
+      title: item.snippet.title,
+      description: item.snippet.description,
+      thumbnail: item.snippet.thumbnails.default.url,
+      url: `https://www.youtube.com/watch?v=${item.id}`
+    }
+  })[0]
+}
+
+export const getRelatedVideos = async (videoId) => {
+  const res = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+    params: {
+      key: process.env.REACT_APP_YOUTUBE_API_KEY,
+      type: 'video',
+      part: 'snippet',
+      relatedToVideoId: videoId,
+      maxResults: 10
+    }
+  })
+
+  return res.data.items.map(item => {
+    return {
+      id: item.id.videoId,
+      title: item.snippet.title,
+      description: item.snippet.description,
+      thumbnail: item.snippet.thumbnails.default.url,
+      url: `https://www.youtube.com/watch?v=${item.id.videoId}`
+    }
+  })
+}
