1f59677e43dcfbdc6b8b1d2fa7fa4cfdcbee4770
Mahmoud Soliman <mahmoudsoliman@Mahmouds-MacBook-Pro.local>
2021-04-08 15:33:13 +0200

    fix build issues

--- src/components/VideosGrid.jsx
+++ src/components/VideosGrid.jsx
@@ -5,14 +5,14 @@ import { searchExecuted } from '../actions/searchActions'
 import { Link } from 'react-router-dom'
 
 export default function VideosGrid() {
-  const query = useSelector(state => state.search.query)
   const videos = useSelector(state => state.search.videos)
-
-  const dispatch = useDispatch()
-
+  
+  
   useEffect(() => {
+    const query = useSelector(state => state.search.query)
+    const dispatch = useDispatch()
     dispatch(searchExecuted(query))
-  }, [query])
+  }, [])
 
   return (
     <div className="card-group row-cols-5">
